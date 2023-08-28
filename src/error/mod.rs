b46727512b94afb34bcced187ca54752a1f1882f
moroviintaas <moroviintaas@gmail.com>
2023-08-28 20:32:11 +0200

    Experimentally introduced SztormRLError.

---
+++ src/error/mod.rs
@@ -0,0 +1,26 @@
+use tch::TchError;
+use thiserror::Error;
+use sztorm::error::SztormError;
+use sztorm::protocol::DomainParameters;
+
+
+#[derive(Error, Debug)]
+pub enum SztormRLError<DP: DomainParameters>{
+    #[error("Basic sztorm error: {0}")]
+    Sztorm(SztormError<DP>),
+    #[error("Torch error: {0}")]
+    Torch(TchError),
+
+}
+
+impl<DP: DomainParameters> From<TchError> for SztormRLError<DP>{
+    fn from(value: TchError) -> Self {
+        Self::Torch(value)
+    }
+}
+
+impl<DP: DomainParameters> From<SztormError<DP>> for SztormRLError<DP>{
+    fn from(value: SztormError<DP>) -> Self {
+        Self::Sztorm(value)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,9 @@ pub mod torch_net;
 pub mod actor_critic;
 pub mod experiencing_policy;
 pub mod tensor_repr;
+pub mod error;
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}

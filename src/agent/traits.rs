80cd7a78d5ce5e5dd36ca0f8ea4499ffe5aea501
moroviintaas <moroviintaas@gmail.com>
2023-10-12 20:28:42 +0200

    Updated to changes in Sztorm with type names in traits

--- src/agent/traits.rs
+++ src/agent/traits.rs
@@ -2,28 +2,28 @@ use sztorm::agent::{AutomaticAgent, AutomaticAgentRewarded, PolicyAgent, Scoring
 use sztorm::domain::DomainParameters;
 use crate::LearningNetworkPolicy;
 
-pub trait NetworkLearningAgent<DP: DomainParameters>: AutomaticAgentRewarded<DP>  + PolicyAgent<DP> + TracingAgent<DP, <Self as StatefulAgent<DP>>::State>
+pub trait NetworkLearningAgent<DP: DomainParameters>: AutomaticAgentRewarded<DP>  + PolicyAgent<DP> + TracingAgent<DP, <Self as StatefulAgent<DP>>::InfoSetType>
     where  <Self as PolicyAgent<DP>>::Policy: LearningNetworkPolicy<DP>,
-    <Self as StatefulAgent<DP>>::State: ScoringInformationSet<DP>
+    <Self as StatefulAgent<DP>>::InfoSetType: ScoringInformationSet<DP>
 {
 }
 
 impl <DP: DomainParameters, T: AutomaticAgentRewarded<DP>  + PolicyAgent<DP>
-+ TracingAgent<DP, <Self as StatefulAgent<DP>>::State>>
++ TracingAgent<DP, <Self as StatefulAgent<DP>>::InfoSetType>>
 NetworkLearningAgent<DP> for T
 where <T as PolicyAgent<DP>>::Policy: LearningNetworkPolicy<DP>,
-<T as StatefulAgent<DP>>::State: ScoringInformationSet<DP>
+<T as StatefulAgent<DP>>::InfoSetType: ScoringInformationSet<DP>
 {
 }
 
 
 pub trait TestingAgent<DP: DomainParameters>: AutomaticAgent<DP>  + PolicyAgent<DP>
- + TracingAgent<DP, <Self as StatefulAgent<DP>>::State>
-where <Self as StatefulAgent<DP>>::State: ScoringInformationSet<DP>{}
+ + TracingAgent<DP, <Self as StatefulAgent<DP>>::InfoSetType>
+where <Self as StatefulAgent<DP>>::InfoSetType: ScoringInformationSet<DP>{}
 
 impl <DP: DomainParameters, T: AutomaticAgent<DP>  + PolicyAgent<DP>
-+ TracingAgent<DP, <Self as StatefulAgent<DP>>::State>>
++ TracingAgent<DP, <Self as StatefulAgent<DP>>::InfoSetType>>
 
 TestingAgent<DP> for T
-where <T as StatefulAgent<DP>>::State: ScoringInformationSet<DP>
+where <T as StatefulAgent<DP>>::InfoSetType: ScoringInformationSet<DP>
 {}
